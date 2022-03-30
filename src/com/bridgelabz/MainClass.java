2466bd17182837c24e2524e0594a6143f7182535
Akshada <akshumore96@gmail.com>
2022-03-30 17:18:54 +0530

    Added word frequency

---
+++ src/com/bridgelabz/MainClass.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class MainClass {
+    public static void main(String[] args){
+        HashTable h = new HashTable();
+        String str = "To be or not to be";
+        String[] arr = str.split(" ");
+        for (String key : arr) {
+            h.add(key);
+        }
+        h.display();
+    }
+}
